c17d9df2971ea8a4c2e7fab6ce1a6c8fed035e2e
marianobuglio <marianobuglio@gmail.com>
2018-05-16 22:02:47 -0300

    H2:  Como un usuario deseo actualizar los datos de un documento, incluida la capacidad de modificar el archivo PDF adjunto.

--- database/migrations/2018_05_13_214739_create_files_table.php
+++ database/migrations/2018_05_13_214739_create_files_table.php
@@ -14,6 +14,7 @@ class CreateFilesTable extends Migration
     {
         Schema::create('files', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
             $table->string('url');
             $table->string('mime');
             $table->float('size');
